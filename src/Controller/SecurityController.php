46b213e71036cc4937df56bbeccfca822b349287
Nicolas DOERR <you@example.com>
2021-02-26 09:51:28 +0100

    resolution des problemes d'affichage 1

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,12 +33,8 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-<<<<<<< HEAD
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,
         'countrys' => $countrys]);
-=======
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
->>>>>>> cc601a5077ba7742a4f6fae72053eedf898f9fb8
     }
 
     /**
