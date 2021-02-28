fe0c0ab9efe6b7a82c86a2706098db1807ecd7f5
unknown <nemorluther@gmail.com>
2021-02-28 16:00:26 +0100

    mise a jours bdd

--- src/Controller/CountryPageController.php
+++ src/Controller/CountryPageController.php
@@ -35,53 +35,12 @@ class CountryPageController extends AbstractController
         
        //création du formulaire 
         $publication=new Publication();
-        $form =$this->createFormBuilder($publication)
-                
-                ->add('countryName',TextType::class, [
-                    'label' => 'Destination',
-                    
-                        
-                ])
-                        
-                ->add('date', BirthdayType::class,[
-                    'required' => true,
-                    'label' => 'Date de départ',
-                    'placeholder' => [
-                        'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
-                    ]
-                     
-                ])
-                ->add('duration', DateIntervalType::class, [
-                    'input'      => 'string', // render a text field for each part
-                    // 'input'    => 'string',  // if you want the field to return a ISO 8601 string back to you
-                
-                    // customize which text boxes are shown
-                    'with_years'  => false,
-                    'with_months' => true,
-                    'with_days'   => true,
-                ])
-                ->getForm();
-                
-    
         
-        
-        $form->handleRequest($request);
-        
-        if($form->isSubmitted() && $form->isValid()){
-            //récuperer les tokens de l'utilisateur connecté
-            $user = $this->get('security.token_storage')->getToken()->getUser();
-            $publication->setUser($user);
-            
-            $manager= $this->getDoctrine()->getManager();
-            $manager->persist($publication);
-            $manager->flush();
-        }
 
         return $this->render('country_page/index.html.twig', [
             'country' => $country,
             'publications' => $publications,
             'countrys' => $countrys,
-            'formPublication' => $form->createView(),
             'user' => $user,
             'publication' => $publication
         ]);
